0041df16cab053125888bbbe13f7e15a6d11f277
AND.EVV <andrew60199@gmail.com>
2022-12-07 20:42:24 +1100

    railway attempt 2

--- config/connection.js
+++ config/connection.js
@@ -9,11 +9,17 @@ const Sequelize = require('sequelize');
 //       dialect: 'mysql',
 //     });
 
-const PORT = process.env.PORT || 3001
+// const PORT = process.env.PORT || 3001
 
-const sequelize = `0.0.0.0:${PORT}`
-  ? new Sequelize(`0.0.0.0:${PORT}`)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+// const sequelize = `0.0.0.0:${PORT}`
+//   ? new Sequelize(`0.0.0.0:${PORT}`)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//     });
+
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
     });
